f64d4fa42cb72234af0b18bf47d38d74a080eb5e
kubackikacper <kacper1048@gmail.com>
2024-12-24 14:12:30 +0100

    24.12.2024

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string pangram =
+    "William who was a widower told his son he could believe he'd been taken in by the conmen";
+Console.WriteLine(pangram);
+string[] message = pangram.Split(' ');
+
+string[] message2 = new string[message.Length];
+
+for (int i = 0; i < message.Length; i++)
+{
+    char[] letters = message[i].ToCharArray();
+    Array.Reverse(letters);
+    message2[i] = new string(letters);
+}
+string result = String.Join(" ", message2);
+Console.WriteLine(result);
+Console.ReadLine();
---
+++ README.md
@@ -0,0 +1,5 @@
+# C# Console Application
+## Simple application that reverses given string.
+### Example:
+### string in = "William who was a widower told his son he could believe he'd been taken in by the conmen";
+### string out = "mailliW ohw saw a rewodiw dlot sih nos eh dluoc eveileb d'eh neeb nekat ni yb eht nemnoc";
